01d07d18a5ad7589aded11c6bb69c2286f4d2824
sidythakur <34541410+sidythakur@users.noreply.github.com>
2018-11-14 16:07:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,4 @@
-'''from flask import Flask
-from flask import render_template, request
-from datetime import datetime
-app = Flask(__name__)
 
-@app.route("/")
-def home():
-    content="xyz"
-    return render_template("home.html",title="Hello, Flask",content=content)
-@app.route("/result", methods=['POST','GET'])
-def result():
-    if request.method =='POST':
-        ph=request.form
-        print(ph)
-        return render_template(result.html)
-if __name__ == "__main__":
-    app.run(debug=True)  '''
 from flask import Flask, render_template, request
 from trial import unfound
 app = Flask(__name__)
